5c0deace404a1fd52c5b4eb28fc548beea80ce27
Aries-Hui <937244334@qq.com>
2020-12-20 17:47:23 +0800

    wx-f2初实践

---
+++ miniprogram/pages/check/check.ts
@@ -0,0 +1,114 @@
+// miniprogram/pages/check/check.js
+let chart = "" as any;
+
+function chartOption(data: any) {
+    chart.source(data, {
+        date: {
+            range: [0, 1],
+            type: 'timeCat',
+            mask: 'MM-DD'
+        },
+        value: {
+            max: 300,
+            tickCount: 4
+        }
+    });
+    chart.area().position('date*value').color('city').adjust('stack');
+    chart.line().position('date*value').color('city').adjust('stack');
+    chart.render();
+}
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        onInitChart(F2: any, config: any) {
+            chart = new F2.Chart(config);
+
+            const data = [
+                {value: 63.4, city: 'New York', date: '2011-10-01'},
+                {value: 62.7, city: 'Alaska', date: '2011-10-01'},
+                {value: 72.2, city: 'Austin', date: '2011-10-01'},
+                {value: 58, city: 'New York', date: '2011-10-02'},
+                {value: 59.9, city: 'Alaska', date: '2011-10-02'},
+                {value: 67.7, city: 'Austin', date: '2011-10-02'},
+                {value: 53.3, city: 'New York', date: '2011-10-03'},
+                {value: 59.1, city: 'Alaska', date: '2011-10-03'},
+                {value: 69.4, city: 'Austin', date: '2011-10-03'},
+            ]
+
+            chartOption(data)
+
+            // 注意：需要把chart return 出来
+            return chart;
+        },
+    },
+
+    bindButton: function() {
+        console.log(111);
+        chart.clear(); // 清除
+        const data = [
+            {value: 10.4, city: 'New York', date: '2011-10-01'},
+            {value: 62.7, city: 'Alaska', date: '2011-10-01'},
+            {value: 72.2, city: 'Austin', date: '2011-10-01'},
+            {value: 4, city: 'New York', date: '2011-10-02'},
+            {value: 59.9, city: 'Alaska', date: '2011-10-02'},
+            {value: 67.7, city: 'Austin', date: '2011-10-02'},
+            {value: 9, city: 'New York', date: '2011-10-03'},
+            {value: 59.1, city: 'Alaska', date: '2011-10-03'},
+            {value: 69.4, city: 'Austin', date: '2011-10-03'},
+        ]
+        chartOption(data)
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        console.log(options);
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+})
